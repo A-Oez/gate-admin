74d49f92cccc3f373cbd023ce6e5444226199523
AOez <oezahm60@gmail.com>
2025-04-29 18:57:22 +0200

    added login page

---
+++ src/components/LoggingNavbar.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="container-fluid">
+  <nav style="margin-right: 3%;">
+    <ul>
+      <img
+        src="/src/assets/logo.png"
+        style="width: 140px; height: 120px"
+      />
+      <h1>go-gate</h1>
+    </ul>
+    <ul>
+      <li><a href="https://github.com/A-Oez/go-gate"><kbd>GitHub</kbd></a></li>
+    </ul>
+  </nav>
+</div>
+<hr />
+</template>
---
+++ src/views/LoggingView.vue
@@ -0,0 +1,61 @@
+<template>
+  <Navbar />
+  <main class="container" style="margin-top: 4rem">
+    <div class="grid" style="align-items: center; gap: 3rem">
+      <article
+        style="border: 1px solid rgba(255, 255, 255, 0.3); border-radius: 3rem; padding: 3rem"
+      >
+        <div>
+          <hgroup>
+            <h1>Login</h1>
+            <p>Please login with your email and password. 🔐</p>
+          </hgroup>
+        </div>
+      </article>
+
+      <div>
+        <form @submit.prevent="onLogin">
+          <fieldset>
+            <label for="email">
+              Email
+              <input
+                id="email"
+                name="email"
+                type="email"
+                required
+                autocomplete="email"
+                v-model="email"
+              />
+            </label>
+
+            <label for="password">
+              Password
+              <input
+                id="password"
+                name="password"
+                type="password"
+                required
+                autocomplete="current-password"
+                v-model="password"
+              />
+            </label>
+          </fieldset>
+
+          <input type="submit" value="Login" class="contrast" />
+        </form>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import Navbar from '../components/LoggingNavbar.vue'
+
+const email = ref('')
+const password = ref('')
+
+function onLogin() {
+  //TODO: create login call
+}
+</script>
